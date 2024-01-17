6c546870f256a7ed83df70cee13770860b2bab60
William/Back-end <108165232+WilliamProgramadorBR@users.noreply.github.com>
2024-01-17 10:56:28 -0300

    Estrutura Do-While

---
+++ CursoC#/EstruturaDeControle/EstrututaDoWhile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoC_.EstruturaDeControle {
+    internal class EstrututaDoWhile {
+        public static void Executar() {
+            // Olá, vamos lá. Diferença entre While e DoWhile, uma delas é sempre uma vez o bloco será executado
+            string entrada;
+
+            do {
+                Console.WriteLine("Qual o seu nome?");
+                entrada = Console.ReadLine();
+
+                Console.WriteLine("Seja bem-vindo {0}", entrada);
+                Console.WriteLine("Deseja continuar?(S/N)");
+                entrada = Console.ReadLine();
+            } while (entrada.ToLower() == "s");
+        }
+    }
+}
